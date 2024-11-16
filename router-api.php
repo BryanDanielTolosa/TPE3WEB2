bc42f4092331a7446d6e71dfc5a8373308822ea8
github_smg2107 <smg2107@hotmail.com>
2024-11-16 17:29:02 -0300

    TPE3 - Agregando controlador y modelo de criadero - Router-api

--- router-api.php
+++ router-api.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'libs/router.php';
 require_once './tpe3/controller/perro.controller.php';
+require_once './tpe3/controller/criadero.controller.php';
 require_once './tpe3/controller/user.api.controller.php';
 
 // crea el router
@@ -10,14 +11,19 @@ $router = new Router();
 // define la tabla de ruteo
     #              endpoint ,  verbo ,     controller  ,       metodo
 
+$router->addRoute('criadero', 'GET', 'CriaderoController', 'listarCategorias');
+$router->addRoute('criadero', 'POST', 'CriaderoController', 'agregarCriadero');
+$router->addRoute('criadero/:id', 'PUT', 'CriaderoController', 'editarCriadero');
+$router->addRoute('criadero/:id', 'GET', 'CriaderoController', 'listarCategoriasById');
+$router->addRoute('criadero/:id', 'DELETE', 'CriaderoController', 'eliminarCriadero');
 
 /*$router->addRoute('usuarios/token', 'GET', 'UserApiController',   'getToken');*/
 
 $router->addRoute('perro', 'GET', 'PerroController', 'getAll');
 $router->addRoute('perro', 'POST', 'PerroController', 'add');
-$router->addRoute('perro/:ID', 'GET', 'PerroController', 'getById');
-$router->addRoute('perro/:ID', 'PUT', 'PerroController', 'update');
-$router->addRoute('perro/ID', 'DELETE', 'PerroController', 'delete');
+$router->addRoute('perro/:id', 'GET', 'PerroController', 'getById');
+$router->addRoute('perro/:id', 'PUT', 'PerroController', 'update');
+$router->addRoute('perro/:id', 'DELETE', 'PerroController', 'delete');
 // rutea
 $router->route($_GET['resource'], $_SERVER['REQUEST_METHOD']);
 ?>
